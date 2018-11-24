1f95aea7b88a908aecc27652c251a50edfafa2d2
Bogdan <shevchenkobn@bitbucket.com>
2018-11-24 17:59:02 +0200

    Добавьте файлы проекта.

---
+++ Hasher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
