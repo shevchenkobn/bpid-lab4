41d485752468df09bbfb89326360e30290f0d889
Bogdan <shevchenkobn@bitbucket.com>
2018-11-24 20:19:08 +0200

    Implemented slow algorithm (modPow must be replaced).

---
+++ Hasher/Hasher.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Numerics;
+
+namespace Hasher
+{
+    public enum HasherMode : byte
+    {
+        Bit8 = 8, Bit4 = 4, Bit2 = 2
+    }
+    public static class Hasher
+    {
+        private const int readLimit = 255;
+        private static readonly BigInteger modulus = new BigInteger(1) << ((readLimit - 1) * 8);
+        public static byte CalculateCheckSum(Stream stream, HasherMode mode = HasherMode.Bit8)
+        {
+            if (!stream.CanRead)
+            {
+                throw new ArgumentException("sosi pisos so svoim nechitayemim strimom");
+            }
+
+            var power = readLimit;
+            var buffer = new byte[readLimit];
+            BigInteger sum = 0;
+            var oldPosition = stream.Position;
+            while (stream.Position < stream.Length)
+            {
+                stream.Read(buffer, 0, buffer.Length);
+
+                var curr = new BigInteger(buffer);
+                sum = sum + BigInteger.ModPow(curr, power, modulus);
+            }
+            stream.Seek(oldPosition, SeekOrigin.Begin);
+
+            byte checkSum = 0;
+            var byteSum = sum.ToByteArray();
+            foreach (var b in byteSum)
+            {
+                //checkSum += (byte)((((b & 128) >> 7) + ((b & 64) >> 6) + ((b & 32) >> 5) + ((b & 16) >> 4)
+                //    + ((b & 8) >> 3) + ((b & 4) >> 2) + ((b & 2) >> 1) + (b & 1)) & 1);
+                checkSum ^= b;
+            }
+
+            return ShortenCheckSum(checkSum, mode);
+        }
+
+        public static byte ShortenCheckSum(byte checkSum, HasherMode mode)
+        {
+            if (mode < HasherMode.Bit8)
+            {
+                var bigCheckSum = checkSum;
+                checkSum = 0;
+                var step = (byte)(8 / (int)mode);
+                for (byte i = 0, limit = (byte)mode; i < limit; ++i)
+                {
+                    byte bitSum = 0;
+                    for (byte j = i; j < 8; j += step)
+                    {
+                        bitSum += (byte)((bigCheckSum >> j) & 1);
+                    }
+                    checkSum = (byte)((checkSum << 1) + (bitSum & 1));
+                }
+            }
+
+            return checkSum;
+        }
+    }
+}
--- Hasher/Program.cs
+++ Hasher/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.IO;
 
 namespace Hasher
 {
     class Program
     {
+        const int Iterations = 10;
+        const int MinimumBatchSize = 100_000;
+        const int MaximumBatchSize = 1_000_000;
+        static string ByteToString(byte b) => Convert.ToString(b, 2).PadLeft(8, '0');
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rnd = new Random();
+            for (var i = 0; i < Iterations; i++)
+            {
+                var bytes = new byte[rnd.Next(MinimumBatchSize, MaximumBatchSize)];
+                Console.WriteLine("Created byte array of {0}. Generating...", bytes.Length);
+                rnd.NextBytes(bytes);
+                var alteredBytes = new byte[bytes.Length];
+                Buffer.BlockCopy(bytes, 0, alteredBytes, 0, bytes.Length);
+                alteredBytes[rnd.Next(0, bytes.Length)] = (byte)rnd.Next(256);
+
+                Console.WriteLine("Generated. Calculating checksum for source and altered arrays...");
+                var checkSum = Hasher.CalculateCheckSum(new MemoryStream(bytes));
+                var alteredCheckSum = Hasher.CalculateCheckSum(new MemoryStream(alteredBytes));
+
+                Console.WriteLine("\n8 bits:\n{0}\n{1}", ByteToString(checkSum), ByteToString(alteredCheckSum));
+                Console.WriteLine(
+                    "\n4 bits:\n{0}\n{1}",
+                    ByteToString(Hasher.ShortenCheckSum(checkSum, HasherMode.Bit4)),
+                    ByteToString(Hasher.ShortenCheckSum(alteredCheckSum, HasherMode.Bit4))
+                );
+                Console.WriteLine(
+                    "\n2 bits:\n{0}\n{1}",
+                    ByteToString(Hasher.ShortenCheckSum(checkSum, HasherMode.Bit2)),
+                    ByteToString(Hasher.ShortenCheckSum(alteredCheckSum, HasherMode.Bit2))
+                );
+                Console.WriteLine("\n{0}\n", new string('*', 16));
+            }
+
+            Console.ReadKey();
         }
     }
 }
